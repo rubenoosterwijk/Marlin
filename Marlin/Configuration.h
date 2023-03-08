8d9b6c6f0c1678e7c229dbead73d27310a38809f
Ruben Oosterwijk <rubenoosterwijk@betronic.nl>
2023-03-08 15:37:32 +0100

    TMC debug enable

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -149,7 +149,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MK3S"
+#define CUSTOM_MACHINE_NAME "MK3S+ Bear Mosquito BT002"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
